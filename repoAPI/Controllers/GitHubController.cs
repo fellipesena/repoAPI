a4fde2c09642b657e0962ad1a0019187b38603b3
Fellipe Sena <pfosena@gmail.com>
2020-09-05 02:26:40 -0300

    Criado Controller para teste

---
+++ repoAPI/Controllers/GitHubController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using repoAPI.Services;
+
+namespace repoAPI.Controllers
+{
+    public class GitHubController : Controller
+    {
+        private readonly IGitHubService _githubService;
+        public GitHubController(IGitHubService githubService)
+        {
+            _githubService = githubService;
+        }
+
+        [HttpGet("users/{username}")]
+        public async Task<IActionResult> GetUser(string username) {
+            Console.WriteLine("Entrou aqui");
+            var user = await _githubService.GetUser(username);
+            return user != null ? (IActionResult) Ok(user) : NotFound();
+        }
+    }
+}
