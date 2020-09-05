05fe3c810f0f86e83313ac5d2f3cff78868af9d5
Fellipe Sena <pfosena@gmail.com>
2020-09-05 02:44:39 -0300

    Models criados

--- repoAPI/Models/Repository.cs
+++ repoAPI/Models/Repository.cs
@@ -6,6 +6,11 @@ using System.Threading.Tasks;
 
 namespace repoAPI.Models
 {
+    public class Owner
+    {
+        [JsonProperty("login")]
+        public string Login { get; set; }
+    }
     public class Repository
     {
         [JsonProperty("id")] public string Id { get; set; }
@@ -14,8 +19,8 @@ namespace repoAPI.Models
         [JsonProperty("description")] public string Description { get; set; }
         [JsonProperty("language")] public string Language { get; set; }
         [JsonProperty("updated_at")] public string Lastupdate { get; set; }
-        [JsonProperty("owner").("login")] public string Owner { get; set; }
+        [JsonProperty("owner")] public Owner Owner { get; set; }
         [JsonProperty("avatar_url")] public List<Contributor> Contribuitors { get; set; }
-        [JsonProperty("avatar_url")] public bool Favourite { get; set; }
+        public bool Favourite { get; set; }
     }
 }
